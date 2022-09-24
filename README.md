785177c904982708d84b92f2d6aff8442be6ac68
algomachine007 <benneth@bejamas.io>
2022-09-24 18:51:58 +0100

    checkout is unavailable for test shops

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 // install shopify-buy
 npm install shopify-buy
+
+https://shopify.github.io/js-buy-sdk/
--- pages/collection/[collection].tsx
+++ pages/collection/[collection].tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { client, parseShopifyResponse } from './../../lib/shopifyClient';
 const Collections = ({ collection }) => {
+  const [qty, setQty] = useState(100);
+
+  console.log(client);
+
+  // const addToCart = () => {
+  //   // Create an empty checkout
+  //   client.checkout.create().then((checkout) => {
+  //     // Do something with the checkout
+  //     console.log(checkout);
+  //   });
+  // };
   return (
     <div>
       <h2>{collection.title}</h2>
       <h4>{collection.description}</h4>
+
+      {/* <button onClick={addToCart} style={{ background: 'teal', width: 60 }}>
+        Checkout
+      </button> */}
     </div>
   );
 };
